3dd483e3ad83ddeb2729fb3aabc9d6bcb75ab906
Anshuman <apcbadal@gmail.com>
2021-07-19 20:13:55 +0530

    adding paypal support

---
+++ src/screens/paypal.js
@@ -0,0 +1,8 @@
+import {} from 'react';
+import paypal from 'paypal-rest-sdk'
+paypal.configure({
+  'mode':'sandbox',
+  'client_id':'Adg5qqM4C6MzhZSkKt3hktnIW5DW8IwTDKB1DaMFWoGn5I7knG6Yq-IzG8MwDiAZ9U542N3fSZoYq9n6',
+  'client_secret':'EDBM80kYF6PXCuyR8JrzR6imb9HoFiOnw-s3Df7A4wSUCszE2ai8iROe8uREvQXnhJa1x_PY7es0s0UX'
+})
+
